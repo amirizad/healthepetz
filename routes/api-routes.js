0930877823539b2aeb0de6eabd3f4e05507baf27
bryancn2009 <bryancn_2009@yahoo.com>
2017-07-08 13:50:18 -0700

    commented code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,23 @@
 module.exports = (express,passport,db,bcrypt)=>{
+    //Declare router variable 
     var router = express.Router();
-
+    //Post route for login using authentication middleware with local strategy
     router.post('/login',passport.authenticate('local',{
+        //if valid redirect to home
         successRedirect:'/',
+        //if not redirect back to login and pass in flash errors
         failureRedirect:'/login',
         failureFlash:true
     }));
-
+    //Post route for register
     router.post('/register',(req,res,next)=>{
+        //if the userpassword matches continue
         if(req.body.password === req.body.repassword){
+            //Declare number of salt rounds default:10
             var saltRounds = 10;
+            //Hash the normal password for protected and pass it as hash
             bcrypt.hash(req.body.password, saltRounds, (err, hash) =>{
+                //Query the Users table and pass in the required fields including the hash pw
                 db.Users.create({
                     "username": req.body.username,
                     "first_name": req.body.fname,
@@ -18,11 +25,14 @@ module.exports = (express,passport,db,bcrypt)=>{
                     "email": req.body.email,
                     "password": hash
                 }).then((data)=>{
+                    //When done grab the new user id and store it in a variable
                     const user_id = data.dataValues.id;
+                    //login the user with the id and redirect to home page
                     req.login(user_id,(err)=>{
                         res.redirect('/');
                     });
                 }).catch((err)=>{
+                    //if there is any errors render register and pass in the error flash msgs
                     res.render('register',{
                         username: req.body.username,
                         fname: req.body.fname,
@@ -33,10 +43,17 @@ module.exports = (express,passport,db,bcrypt)=>{
                 });
             });
         } else {
+            //else render register page again with filled values and flash msg
             res.render('register',{
+                username: req.body.username,
+                fname: req.body.fname,
+                lname: req.body.lname,
+                email: req.body.email,
                 error:[{"message": "Your passwords do not match. Try again."}]
             });
         }
     });
+
+    //returns router back to request
     return router;
 };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,29 +1,38 @@
 module.exports = (express,passport,db)=>{
+    
+    //Declare router and auth variables
     const router = express.Router();
     const auth = require('./../config/passport/passport.js')(passport,db);
 
+    //Get home route only if user is Authenticated using middleware
     router.get('/',auth(),(req,res,next)=>{
         res.render('index');
     });
 
+    //Get login page and verify that...
     router.get('/login',(req,res,next)=>{
+        //if the user is authenticated send them back to home
         if(req.isAuthenticated()){
             res.redirect('/');
         } else {
+            //else render login page and if there is any flash msgs render them too
             res.render('login',{
                 error:req.flash('error')
             });
         }
     });
 
+    //Get register will render register
     router.get('/register',(req,res,next)=>{
         res.render('register');
     });
 
+    //Get logout will simply logout the user using logout();
     router.get('/logout',(req,res)=>{
         req.logout();
         res.redirect('/');
     });
 
+    //returns the router requsted
     return router;
 };
