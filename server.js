ec4a2e9ac0162da5df772401a819faa19474279a
bryancn2009 <bryancn_2009@yahoo.com>
2017-07-08 20:00:25 -0700

    stabalized code merge

--- server.js
+++ server.js
@@ -1,39 +1,100 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+// NPM General Package Dependencies
+// =============================================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const hbs = require('express-handlebars');
+const db = require('./models');
 var methodOverride = require("method-override");
-var handlebars = require("handlebars");
-var expressHandlebars = require("express-handlebars");
 
+// Authentication Dependencies
+// =============================================================
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const MySQLStore = require('express-mysql-session')(session);
+const bcrypt = require('bcrypt');
+const flash = require('connect-flash');
 
-var app = express();
-var PORT = process.env.PORT || 3000;
+// Declare variables & local required files
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+const index = require('./routes/html-routes.js')(express,passport,db,path);
+const API = require('./routes/api-routes.js')(express,passport,db,bcrypt);
 
-//include the routes file
-var routes = require('./routes/html-routes');
-var api = require('./routes/api-routes');
+//Used to create the sessions table for authentication
+const sessionStore = new MySQLStore({
+    host:'localhost',
+    port:'3306',
+    user:'root',
+    password:'root',
+    database:'healthepetz_db'
+});
 
-// Use routes
-app.use('/', routes);
-// Namespace the api routes
-app.use('/api', api);
+// START Configuring Express App
+// ========================================================================================
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(path.join(__dirname + "/public/")));
-
-// Parse incoming requests as a JSON object
+// Sets up the Express app to handle data & cookie parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+app.use(cookieParser());
+app.use(flash());
 app.use(methodOverride("_method"));
 
-// Configure and set handlebars as the view engine
-app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// Sets up the Handlebars as default view engine with express
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
+//Creates the sessions table that will authenticate user sessions
+app.use(session({
+    //random string that gets hashed to validate a real session and not a spoof
+    secret: 'the intense secret code you will ever see',
+    //saves a new session on every request if needed
+    resave: false,
+    //database location of saved sessions to avoid having to login everytime 
+    //the server is restarted so that it is not stored locally
+    store: sessionStore,
+    //if the user is not logged in don't save a session or add a cookie
+    saveUninitialized: false,
+    //To be enabled if using HTTPS
+    //   cookie: { secure: true }
+}));
 
-app.listen(PORT, () => {
-    console.log(`The server is listening on port: ${PORT}`);
+//Sets up passport with express
+app.use(passport.initialize());
+app.use(passport.session());
+const localStrategy = require('./config/passport/passport-strategy.js')(passport,LocalStrategy,db,bcrypt);
+
+//Servers public content such as CSS Javascript required in the HTML files
+app.use(express.static(path.join(__dirname,'public')));
+
+//Sets up express routes
+app.use('/',index);
+app.use('/api',API);
+
+//Sets up express to handle 404 NOT FOUND
+app.use((req, res)=> {
+    res.status(404).send('404: Sorry the page you requested is not on this server.');
 });
+
+//Sets up express to handle 500 INTERNAL SERVER ERROR
+app.use(function(error, req, res) {
+    res.status(500).send('500: Internal Server Error');
+});
+
+//END CONFIGURE
+//=================================================================================
+
+// Sync Database and Start the Server
+// =============================================================
+db.sequelize.sync().then(()=>{
+    app.listen(PORT,()=>{
+        console.log('SERVER STARTED ON PORT ' + PORT);
+    });
+});
+
+
