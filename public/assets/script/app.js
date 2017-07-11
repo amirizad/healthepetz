af9396027c0760997443218e089a51a17a2530d6
amirizad <info@amirizad.com>
2017-07-11 12:08:00 -0700

    Some minor changes to fron end

--- public/assets/script/app.js
+++ public/assets/script/app.js
@@ -29,46 +29,47 @@ $(document).ready(function() {
     //             "visible": false
     //         }
     //     ]
-  } );
+  	// });
+});
 
-	$(".submitbtn").click(function(event){
-		event.preventDefault();
-		var verified=1;
-		var $error="";
-		var $form = $(this).closest('form');
-    var formId = $form.attr("id");
-    var $action = $form.attr("action");
-		var $errmsg = $form.find('.message').attr('id');
-		$('#' + formId + ' [required]').each(function(){
-			var elemval=$.trim($(this).val());
-			var elempatt=$(this).attr('pattern');
-			if (elemval === "" || !elemval.match(elempatt)){
-				verified=0;
-				$error=$(this).attr('id');
-				return false;
+$(".submitbtn").click(function(event){
+	event.preventDefault();
+	var verified=1;
+	var $error="";
+	var $form = $(this).closest('form');
+	var formId = $form.attr("id");
+	var $action = $form.attr("action");
+	var $errmsg = $form.find('.message').attr('id');
+	$('#' + formId + ' [required]').each(function(){
+		var elemval=$.trim($(this).val());
+		var elempatt=$(this).attr('pattern');
+		if (elemval === "" || !elemval.match(elempatt)){
+			verified=0;
+			$error=$(this).attr('id');
+			return false;
+		};
+	});
+	if(verified===1){
+		$('.message').css('display','none');
+		$.post($action, $(this.form).serialize() , function(data, status){
+			if(status="success"){
+				var stat = data;
+				if(stat.length>3){
+					$("#user_idx").val(stat);
+					$("#submitbtn").val("login");
+					$("#portalx").val("system");
+					$form.submit();						
+				}
 			};
 		});
-		if(verified===1){
-			$('.message').css('display','none');
-			$.post($action, $(this.form).serialize() , function(data, status){
-				if(status="success"){
-					var stat = data;
-					if(stat.length>3){
-						$("#user_idx").val(stat);
-						$("#submitbtn").val("login");
-						$("#portalx").val("system");
-						$form.submit();						
-					}
-				};
-			});
-		}
-		else{
-			$("#"+$errmsg).html('Please fill in the required fields (*)');
-			$("#"+$errmsg).css('display','block');
-			$("#"+$error).focus();
-		};
-	});  
-});
+	}
+	else{
+		$("#"+$errmsg).html('Please fill in the required fields (*)');
+		$("#"+$errmsg).css('display','block');
+		$("#"+$error).focus();
+	};
+});  
+
 
 function signupTab(){
   $('#signuptab').addClass('hide');
