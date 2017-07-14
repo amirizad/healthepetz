16aa1220a3e6b244cad8386f133fdb43f7847f44
Amir Izadoanah <info@amirizad.com>
2017-07-13 19:47:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use(function(error, req, res) {
 
 // Sync Database and Start the Server
 // =============================================================
-db.sequelize.sync().then(()=>{
+db.sequelize.sync({force: true}).then(()=>{
     app.listen(PORT,()=>{
         console.log('SERVER STARTED ON PORT ' + PORT);
     });
