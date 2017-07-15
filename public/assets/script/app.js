e52774cb11e722a9b0eeaf2b0e7d87ee27020eeb
bryancn2009 <bryancn_2009@yahoo.com>
2017-07-15 10:00:00 -0700

    update

--- public/assets/script/app.js
+++ public/assets/script/app.js
@@ -357,5 +357,29 @@ $(document).ready(function() {
     var target = this.href.split('#');
     $('.nav a').filter('[href="#'+target[1]+'"]').tab('show');
 	});
+
+	$('#forgotbtn').on('click',function(e){
+		e.preventDefault();
+		
+		var userEmail = $('#forgotemail').val();
+
+		var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "http://localhost:3000/api/password-reset",
+		"method": "POST",
+		"headers": {
+			"content-type": "application/json",
+			"cache-control": "no-cache",
+		},
+		"processData": false,
+		"data": `{email: ${userEmail}}`
+		}
+
+		$.ajax(settings).done(function (response) {
+		console.log(response);
+		});
+	
+	})
 });
 
